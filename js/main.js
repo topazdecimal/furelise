189686dc752e46cd7dabd55c9bed221a1982f8a5
Topaz Decimal <topaz@garnettmm.com>
2020-03-21 19:32:20 +0100

    yemiadedapo workbox reorder

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$("#js-rotating").Morphext({
+    // The [in] animation type. Refer to Animate.css for a list of available animations.
+    animation: "pulse",
+    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+    separator: ",",
+    // The delay between the changing of each phrase in milliseconds.
+    speed: 3000,
+    complete: function () {
+        // Called after the entrance animation is executed.
+    }
+});
+
+
+$("#js-rotating-2").Morphext({
+    // The [in] animation type. Refer to Animate.css for a list of available animations.
+    animation: "pulse",
+    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+    separator: ",",
+    // The delay between the changing of each phrase in milliseconds.
+    speed: 3000,
+    complete: function () {
+        // Called after the entrance animation is executed.
+    }
+});
+
+sal({
+    threshold: .2,
+    once: true,
+  });
